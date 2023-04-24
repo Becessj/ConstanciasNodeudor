d679febe691699261b6680f17e797457c7a50355
Becessj <144996@unsaac.edu.pe>
2023-04-24 14:47:46 -0500

    add print each one cuenta

--- ApiNotaria/Controllers/UsuariosController.cs
+++ ApiNotaria/Controllers/UsuariosController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Notaria.Context;
-using Notaria.Models;
+using Notaria.Models;       
 using Microsoft.AspNetCore.Cors;
 
 namespace Notaria.Controllers
